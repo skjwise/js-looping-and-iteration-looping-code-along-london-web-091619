7da47542ad335525f1003e8de7a666baae8e0018
skjwise <skjwise@hotmail.com>
2019-10-28 16:41:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names){
+    for (let i = 0; i < names.length; i++) {
+        names[i] = (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return names;
+}
+
+function countDown(startNumber){
+    while (startNumber > 0) {
+        console.log(startNumber);
+        startNumber -= 1;
+    }
+
+    console.log(startNumber);
+}
